7608d8e551fe313f7375d4edcf85000eddfad4de
camariana <camariana40@outlook.com>
2021-01-23 16:00:17 +0000

    add initial solution and test cases

---
+++ index.js
@@ -0,0 +1,9 @@
+function rot13(str) {
+
+    return str;
+  }
+  
+  rot13("SERR PBQR PNZC");
+  rot13("SERR CVMMN!");
+  rot13("SERR YBIR?");
+  rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.");
